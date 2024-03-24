06b6b348a42d9a42898491cb826a71abe14ff51c
Jon Gainza <124215384+jongainza@users.noreply.github.com>
2024-03-23 18:45:24 -0700

    merge

--- bubble.js
+++ bubble.js
@@ -1,3 +1,17 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let swaped = false;
+    for (let j = 0; j < arr.length - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+        swaped = true;
+      }
+    }
+    if (swaped) break;
+  }
+  return arr;
+}
 
 module.exports = bubbleSort;
--- merge.js
+++ merge.js
@@ -1,5 +1,29 @@
-function merge() {}
+function merge(arr1, arr2) {
+  let copy1 = [...arr1];
+  let copy2 = [...arr2];
+  let results = [];
+  let i = 0;
+  let j = 0;
+  while (i < copy1.length && j < copy2.length) {
+    if (copy1[i] <= copy2[j]) {
+      results.push(copy1[i]);
+      i++;
+    } else {
+      results.push(copy2[j]);
+      j++;
+    }
+  }
+  while (i < copy1.length) {
+    results.push(copy1[i]);
+    i++;
+  }
+  while (j < copy2.length) {
+    results.push(copy2[j]);
+    j++;
+  }
+  return results;
+}
 
 function mergeSort() {}
 
-module.exports = { merge, mergeSort};
+module.exports = { merge, mergeSort };
