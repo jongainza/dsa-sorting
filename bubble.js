f3ce4b58c21720a790818fcfed55cfc2248238f2
Jon Gainza <124215384+jongainza@users.noreply.github.com>
2024-03-24 07:36:40 -0700

    new functions implemantation

--- bubble.js
+++ bubble.js
@@ -1,7 +1,7 @@
 function bubbleSort(arr) {
   for (let i = 0; i < arr.length; i++) {
     let swaped = false;
-    for (let j = 0; j < arr.length - i; j++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j + 1];
         arr[j + 1] = arr[j];
